0650e7a9c3b820f15f511328918f6ccff19d6b1b
John Mann <jmann@consumer-edge.com>
2019-01-30 14:37:51 -0500

    updated flask admin

---
+++ admin_example.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/jmann/Projects/admin/admin.db'
+app.config['SECRET_KEY'] = 'mysecret'
+
+db = SQLAlchemy(app)
+
+admin = Admin(app)
+
+class Person(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(30))
+
+admin.add_view(ModelView(Person, db.session))
+
+if __name__ == '__main__':
+	app.run(debug=True)
